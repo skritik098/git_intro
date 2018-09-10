4083be45d11679cda52d8870884fa77e5954e24c
skritik98 <s.kritik440@gmail.com>
2018-09-10 18:44:45 +0530

    sheldon greeter

--- file01.js
+++ file01.js
@@ -1,5 +1,7 @@
 function greet(name){
-    console.log('hello world!'+name)
+    console.log('knock knock knock '+name)
+    console.log('knock knock knock '+name)
+    console.log('knock knock knock '+name)
 }
 
 greet('Kritik')
